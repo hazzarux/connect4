67920683a0a897ae3b9e92798af9306276178da5
Yigit Ozkan <yigitozkan2804@gmail.com>
2013-12-16 13:35:00 +0100

    added missing files

---
+++ src/Move.java
@@ -0,0 +1,38 @@
+
+public class Move {
+	/**
+	 * klasse move voor elke move dat een computerspeler kan doen!
+	 */
+	private int punten;
+	private String methode;
+	private int kolom;
+	
+	public Move(String methode){
+		this.methode=methode;
+	}
+	
+	public int getPunten() {
+		return punten;
+	}
+
+	public void setPunten(int punten) {
+		this.punten = punten;
+	}
+
+	public String getMethode() {
+		return methode;
+	}
+
+	public void setMethode(String methode) {
+		this.methode = methode;
+	}
+
+	public int getKolom() {
+		return kolom;
+	}
+
+	public void setKolom(int kolom) {
+		this.kolom = kolom;
+	}
+	
+}
